f1cbe3bdb6161de3dde7336c42686519efb886d7
Linda Zhao <linda.zhao23@gmail.com>
2021-11-23 12:52:48 -0500

    Combine functions into 1

--- Bridge_Calculations.m
+++ Bridge_Calculations.m
@@ -8,9 +8,8 @@ y_zero = zeros(1,n);        % Initialize y-axis
 
 %% 1. Point Loading Analysis (SFD, BMD)
 P = -185;
-[total_loads] = ApplyPL(550, P, x, total_loads); % Construct load vector
-[total_loads] = ApplyPL(L, P, x, total_loads); % Construct load vector
-[SFD_PL, BMD_PL] = MakeSFDBMD(total_loads); % Construct SFD, BMD
+[SFD_PL, BMD_PL, total_loads] = ApplyPL(550, P, x, total_loads); % Construct load vector
+[SFD_PL, BMD_PL, total_loads] = ApplyPL(L, P, x, total_loads); % Construct load vector
 plot(x, SFD_PL)
 plot(x, BMD_PL)
 set(gca,'YDir','reverse')
@@ -36,11 +35,12 @@ TauU = 4;
 TauG = 2;
 mu = 0.2;
 
-function [ total_loads ] = ApplyPL( xP, P, x, total_loads )
-% Constructs load vector from application of one point load. Assumes fixed location of supports
+function [ SFD_PL, BMD_PL, total_loads ] = ApplyPL( xP, P, x, total_loads )
+% Constructs load vector from application of one point load, then SFD & BMD from total loads.
+% Assumes fixed location of supports.
 % Input: location and magnitude of point load. The previous load vector can be entered as input to 
 % construct the load vector of multiple point loads.
-% Output: total_loads, 1-D array of length n
+% Output: SFD_PL, BMD_PL, total_loads: 1-D arrays of length n
 % xP = distance from left (A) support (x = 0)
 xP_B = 1060;
 cur_P_B = -P * xP / xP_B; % P_B due to current P
@@ -50,15 +50,11 @@ P_B = cur_P_B + total_loads(xP_B+1) - total_loads(xP_B); % Overall P_B
 total_loads(1) = P_A;
 total_loads(xP) = P;
 total_loads(xP_B+1) = P_B;
-end
 
-function [ SFD_PL, BMD_PL ] = MakeSFDBMD(total_loads)
 % Constructs SFD, BMD from all point loads applied.
-% Input: total_loads vector containing the current loading of the beam.
-% Output: SFD_PL, BMD_PL, both 1-D arrays of length n.
-    n = 1251;
-    SFD_PL = zeros(1, n);
-    SFD_PL(1) = total_loads(1);
+n = 1251;
+SFD_PL = zeros(1, n);
+SFD_PL(1) = total_loads(1);
     for i = 2:length(total_loads)
         SFD_PL(i) = total_loads(i) + SFD_PL(i-1);
     end
