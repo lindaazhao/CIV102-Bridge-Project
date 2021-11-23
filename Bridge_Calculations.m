9043030fabfc5e12efe6f4b7bab377dd92bf8e84
Linda Zhao <linda.zhao23@gmail.com>
2021-11-23 10:44:40 -0500

    Clean up functions

--- Bridge_Calculations.m
+++ Bridge_Calculations.m
@@ -1,19 +1,16 @@
 % 0. Initialize Parameters
 
-n = 1251;               % Number of locations to evaluate bridge failure
-L = 1250;               % Length of bridge
-x = linspace(0, L, n);  % Define x coordinate
-% SFD_PL = zeros(1, n)    % Initialize SFD(x)
-total_loads = zeros(1, n); % Initialize total_loads for SFD(x)
-y_zero = zeros(1,n);    % Initialize y-axis
-BMD_PL = zeros(1,n);    % Initialize BMD(x)
+n = 1251;                   % Number of locations to evaluate bridge failure
+L = 1250;                   % Length of bridge
+x = linspace(0, L, n);      % Define x coordinate
+total_loads = zeros(1, n);  % Initialize total_loads for SFD(x)
+y_zero = zeros(1,n);        % Initialize y-axis
 
 %% 1. Point Loading Analysis (SFD, BMD)
 P = -185;
-[total_loads, BMD_PL] = ApplyPL(550, P, x, total_loads); % Construct SFD, BMD
-[total_loads, BMD_PL] = ApplyPL(L, P, x, total_loads); % Construct SFD, BMD
-SFD_PL = MakeSFD(total_loads)
-BMD_PL = MakeBMD(SFD_PL)
+[total_loads] = ApplyPL(550, P, x, total_loads); % Construct load vector
+[total_loads] = ApplyPL(L, P, x, total_loads); % Construct load vector
+[SFD_PL, BMD_PL] = MakeSFDBMD(total_loads); % Construct SFD, BMD
 plot(x, SFD_PL)
 plot(x, BMD_PL)
 set(gca,'YDir','reverse')
@@ -38,11 +35,12 @@ E = 4000;
 TauU = 4;
 TauG = 2;
 mu = 0.2;
-%%
-function [ total_loads, BMD_PL ] = ApplyPL( xP, P, x, total_loads )
-% Constructs SFD and BMD from application of 1 Point Load. Assumes fixed location of supports
-% Input: location and magnitude of point load. The previous SFD can be entered as input to construct SFD of multiple point loads
-% Output: SFD, BMD both 1-D arrays of length n
+
+function [ total_loads ] = ApplyPL( xP, P, x, total_loads )
+% Constructs load vector from application of one point load. Assumes fixed location of supports
+% Input: location and magnitude of point load. The previous load vector can be entered as input to 
+% construct the load vector of multiple point loads.
+% Output: total_loads, 1-D array of length n
 % xP = distance from left (A) support (x = 0)
 xP_B = 1060;
 cur_P_B = -P * xP / xP_B; % P_B due to current P
@@ -52,18 +50,17 @@ P_B = cur_P_B + total_loads(xP_B+1) - total_loads(xP_B); % Overall P_B
 total_loads(1) = P_A;
 total_loads(xP) = P;
 total_loads(xP_B+1) = P_B;
-BMD_PL = 0;
 end
 
-function [SFD_PL] = MakeSFD(total_loads)
+function [ SFD_PL, BMD_PL ] = MakeSFDBMD(total_loads)
+% Constructs SFD, BMD from all point loads applied.
+% Input: total_loads vector containing the current loading of the beam.
+% Output: SFD_PL, BMD_PL, both 1-D arrays of length n.
     n = 1251;
     SFD_PL = zeros(1, n);
     SFD_PL(1) = total_loads(1);
     for i = 2:length(total_loads)
         SFD_PL(i) = total_loads(i) + SFD_PL(i-1);
     end
-end
-
-function [BMD_PL] = MakeBMD(SFD_PL)
     BMD_PL = cumsum(SFD_PL);
 end
