4c0fc058dda07fca3b9e4a9a4bbd76d12c13eb10
Linda Zhao <linda.zhao23@gmail.com>
2021-11-25 00:59:02 -0500

    Start SectionProperties(), clean & comment code

--- Bridge_Calculations.m
+++ Bridge_Calculations.m
@@ -1,18 +1,15 @@
-% 0. Initialize Parameters
-
+%% 0. Initialize Parameters
 n = 1251;                   % Number of locations to evaluate bridge failure
 L = 1250;                   % Length of bridge
 x = linspace(0, L, n);      % Define x coordinate
 total_loads = zeros(1, n);  % Initialize total_loads for SFD(x)
-y_zero = zeros(1,n);        % Initialize y-axis
-
+ybar = zeros(1, n);         % Initialize section properties: ybar(x) from bottom
+I = zeros(1, n);            % I(x)
+Q = zeros(1, n);            % Q(x)
 %% 1. Point Loading Analysis (SFD, BMD)
-P = -185;
+P = -185; % Change this variable
 [SFD_PL, BMD_PL, total_loads] = ApplyPL(550, P, x, total_loads); % Construct load vector
 [SFD_PL, BMD_PL, total_loads] = ApplyPL(L, P, x, total_loads); % Construct load vector
-plot(x, SFD_PL)
-plot(x, BMD_PL)
-set(gca,'YDir','reverse')
 
 %% 1.1 Train Load
 n = 1251;                   % Number of locations to evaluate bridge failure
@@ -23,17 +20,21 @@ y_zero = zeros(1,n);        % Initialize y-axis
 
 P = -400;
 P_each = P/6;
-for n = 52:1250-52-856
+for n = 52:1250
     x0=n;
     x_load = [x0,x0+176,x0+340,x0+516,x0+680,x0+856];
     for i=1:6
-        [SFD_PL, BMD_PL, total_loads] = ApplyPL(x_load(i), P_each, x, total_loads);
+        if x_load(i)<=1250
+            [SFD_TL, BMD_TL, total_loads] = ApplyPL(x_load(i), P_each, x, total_loads);
+        end
     end
 subplot(2,1,1);
-plot(x, SFD_PL,'red')
+plot(x, SFD_TL,'red')
+plot(x, SFD_PL) % Plot point load SFD on same graph
 hold on
 subplot(2,1,2);
-plot(x, BMD_PL,'blue')
+plot(x, BMD_TL,'blue')
+plot(x, BMD_PL) % Plot point load BMD on same graph
 set(gca,'YDir','reverse')
 hold on
 SFD_PL = zeros(1,1251);
@@ -41,27 +42,29 @@ BMD_PL = zeros(1,1251);
 total_loads = zeros(1,1251);
 end
 
-%% 2. Define cross-sections
-% There are many (more elegant ways) to construct cross-section objects
+%% 2. Define Cross-Sections
+% Parameters for left half before drastic change (?)
 xc = [0 550 L]; % Location, x, of cross-section change
-bft = [100 100 100]; % Top Flange Width
+tfb = [100 100 100]; % Top Flange Width
 tft = [2.54 2.54 2.54]; % Top Flange Thickness
-hw = [100 120 100]; % Web Height
-tw = [1.27 1.27 1.27]; % Web Thickness (Assuming 2 separate webs)
+wh = [100 120 100]; % Web Height
+wt = [1.27 1.27 1.27]; % Web Thickness (Assuming 2 separate webs)
 bfb = [80 80 80]; % Bottom Flange Width
-tfb = [1.27 1.27 1.27]; % Bottom Flange Thickness
+bft = [1.27 1.27 1.27]; % Bottom Flange Thickness
 a = [400 400 400]; % Diaphragm Spacing
+total_height = tft + wh + bft;
+
 % Optional but you need to ensure that your geometric inputs are correctly implemented
 % VisualizeBridge( {CrossSectionInputs} ); 
-
 %% 3. Define Material Properties
-SigT = 30;
-SigC = 6;
+SigT = 30;  % Tensile strength
+SigC = 6;   % Compressive strength
 E = 4000;
-TauU = 4;
-TauG = 2;
-mu = 0.2;
+TauU = 4;   % Shear strength, matboard
+TauG = 2;   % Shear strength, glue
+mu = 0.2;   % Poisson's ratio
 
+%% Function Definitions
 function [ SFD_PL, BMD_PL, total_loads ] = ApplyPL( xP, P, x, total_loads )
 % Constructs load vector from application of total point loads, then SFD & BMD from total loads.
 % Assumes fixed location of supports.
@@ -72,7 +75,7 @@ function [ SFD_PL, BMD_PL, total_loads ] = ApplyPL( xP, P, x, total_loads )
 xP_B = 1060;
 cur_P_B = -P * xP / xP_B; % P_B due to current P
 P_A = total_loads(1) -P - cur_P_B; % Overall P_A
-P_B = cur_P_B + total_loads(xP_B+1) - total_loads(xP_B); % Overall P_B
+P_B = cur_P_B + total_loads(xP_B+1); % Overall P_B
 
 total_loads(1) = P_A;
 total_loads(xP+1) = P;
@@ -90,4 +93,28 @@ SFD_PL(1) = total_loads(1);
         SFD_PL(i) = total_loads(i) + SFD_PL(i-1);
     end
     BMD_PL = cumsum(SFD_PL);
+end
+
+function [ ybar, I, Q ] = SectionProperties ( )
+% Calculates important sectional properties for left half . Including but not limited to ybar, I, Q, etc.
+% Input: Geometric Inputs. Format will depend on user
+% Output: Sectional Properties at every value of x. Each property is a 1-D array of length n
+tfa = zeros(1, 1251);
+bfa = zeros(1, 1251);
+wa = zeros(1, 1251);
+tfy = zeros(1, 1251);
+wy = zeros(1, 1251); 
+
+for i = 1:length(xc)
+    % Areas, A
+    tfa(i) = bft(i) * tft(i); % Assumes all values have been interpolated already
+    bfa(i) = bfb(i) * tfb(i);
+    wa(i) = 0;
+    % Local centroids, y
+    tfy(i) = total_height - tft(i) / 2;
+    bfy = bft(i) / 2;
+    wy(i) = wh(i)/2 + bft(i);
+    % I naught, I_0
+end
+ybar = (tfa*tfw' + bfa*bfy' + wa*wy') / (tfa + bfa + wa); % This is probably wrong
 end
